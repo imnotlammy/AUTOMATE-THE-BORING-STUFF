6faa0e8ea60bb3713b07dc1e374ce37e78d2b5d3
lemmyn <lemmyenn@gmail.com>
2022-03-15 13:50:17 +0800

    Add 	allMyCats1.py 	conway.py 	enumerate.py 	magic8Ball2.py 	myPets.py 	passingReference.py

--- collatz.py
+++ collatz.py
@@ -1,2 +1,13 @@
-number = int(input())
-def collatz(number):
+print("Enter number:")
+try:
+    number = int(input())
+    while True:
+        if number % 2 == 0:
+            number = number // 2
+            print(number)
+        else:
+            number = 3 * number + 1
+            print(number)
+
+except:
+    print("Please enter an integer")
